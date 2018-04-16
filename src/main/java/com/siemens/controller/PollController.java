3c5f39399ecb64dfc86ad6f5ee7ad98042acd0d9
cesurercan <cesurercan@hotmail.com>
2018-04-16 10:12:53 -0400

    @size and @NotEmpty added to Poll.java

--- src/main/java/com/siemens/controller/PollController.java
+++ src/main/java/com/siemens/controller/PollController.java
@@ -24,6 +24,17 @@ public class PollController {
 	@Inject
 	private PollRepository pollRepository;
 	
+	/**
+	 * A GET request on the /polls endpoint provides a collection of all of the polls available 
+	 * in the QuickPolls application.The @RequestMapping annotation declares the URI and the allowed HTTP method. 
+	 * The getAllPolls() method used ResponseEntity as its return type, indicating that the return value 
+	 * is the complete HTTP response. ResponseEntity gives you full control over the HTTP response, 
+	 * including the response body and response headers. The method implementation begins with reading 
+	 * all of the polls using the PollRepository. We then create an instance of ResponseEntity and pass 
+	 * in Poll data and the HttpStatus.OK status value. The Poll data becomes part of the response body 
+	 * and OK (code 200) becomes the response status code.
+	 */
+	
 	@RequestMapping(value="/polls", method=RequestMethod.GET)
 	public ResponseEntity<Iterable<Poll>> getAllpolls(){
 		Iterable<Poll> allPolls = pollRepository.findAll();
